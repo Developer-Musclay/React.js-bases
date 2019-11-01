676f578b17cccc7b8d0969a2adc01329dd2ecc97
Developer-Musclay <jeremydumascontact@gmail.com>
2019-11-01 14:57:18 +0100

    React-Model ready

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+// == Import : npm
+import React from 'react';
+
+// == Import : local
+import './app.scss';
+
+// == Composant
+const App = () => (
+  <div id="app">
+    <h1>React.js basics</h1>
+  </div>
+);
+
+// == Export
+export default App;
